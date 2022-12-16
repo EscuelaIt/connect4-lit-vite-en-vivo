5413f4bedb0949493716882729540d7b263896fc
Miguel Angel Alvarez <miguel@desarrolloweb.com>
2022-12-16 20:49:34 +0100

    color azul

--- src/components/css/game-css.js
+++ src/components/css/game-css.js
@@ -3,7 +3,7 @@ import { css } from 'lit';
 export const gameCSS = css`
     :host {
         width: 100%;
-        --primary-color: #09A50F;
+        --primary-color: #09A5FF;
         display: flex;
         flex-direction: column;
         justify-content: stretch;
